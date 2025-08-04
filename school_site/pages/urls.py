ec5af81dfe212f7c59c6dccf5715deb7f1b4d98c
Ali-Mubeen-Siddiqui <alimubeensiddiqui@gmail.com>
2025-08-04 14:40:33 +0530

    added routes

--- school_site/pages/urls.py
+++ school_site/pages/urls.py
@@ -6,4 +6,11 @@ urlpatterns = [
     
     path('', views.index,name="home"),
     path('login/', views.login,name="login"),
+    path('about/', views.about, name="about"),
+    path('faculty/', views.faculty, name="faculty"),
+    path('campus/', views.campus, name="campus"),
+    path('programs/', views.programs, name="programs"),
+    path('admissions/', views.admissions, name="admissions"),
+    path('examinations/', views.examinations, name="examinations"),
+    path('contact/', views.contact, name="contact"),
 ]
--- school_site/pages/views.py
+++ school_site/pages/views.py
@@ -7,3 +7,28 @@ def index(request):
 
 def login(request):
     return render(request,"login.html")
+
+def login_view(request):
+    pass
+
+def about(request):
+    return render(request, "about.html")
+
+def faculty(request):
+    return render(request, "faculty.html")
+
+def campus(request):
+    return render(request, "campus.html")
+
+def programs(request):
+    return render(request, "programs.html")
+
+def admissions(request):
+    return render(request, "admissions.html")
+
+def examinations(request):
+    return render(request, "examinations.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
